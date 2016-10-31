3353b79b8e48702536c3641022be1e4c515c5a91
Dzung Tran Xuan <xuandung.hp.vn@gmail.com>
2016-10-31 17:46:11 +0700

    update standard way

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027094934) do
+ActiveRecord::Schema.define(version: 20161031082241) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
